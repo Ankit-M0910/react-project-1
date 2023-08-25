00f0bde32941873300eb1056205339da6e3b2d36
Ankit-M0910 <ankitmallick83@gmail.com>
2023-08-25 21:13:43 +0530

    second commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechyStar</h1>
+            <p>@all right reserved</p>
+        </div>
+
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="https://youtube.com">Youtube</a>
+                <a href="https://instagram.com">Instagram</a>
+                <a href="https://github.com/Ankit-M0910">Github</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
